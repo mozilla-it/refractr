6ab463e7f88ea2a62ee1c90e42b2a09b5010b63c
Scott Idler <sidler@mozilla.com>
2020-05-30 12:19:21 -0700

    adjust docker image and add deployed, version json
    - created refractr/image/
    - moved refractr/refractr.sh -> refractr/image/refractr.sh
    - added cfg.py to handle config values and allow env var passthrus
    - added deployed and version tasks to create json files in image/
    - Dockerfile to COPY image/ -> /refractr/; nginx/ -> /etc/nginx/
    - updated Dockerfile ENTRYPOINT /refractr/refractr.sh
    - moved logic into SHOULD_PUBLISH property
    - simplified refractr image name in docker-compose
    - publish now creates a tag for the ecr repository name
    - .gitignore for generated files: deployed, version, ingress.yaml

---
+++ refractr/image/refractr.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+ACTION=$1
+# Note: this is not bash, but sh instead
+# POSIX "oneliner" to get script's directory
+# https://stackoverflow.com/a/43919044
+a="/$0"; a=${a%/*}; a=${a#/}; a=${a:-.}; DIR=$(cd "$a"; pwd)
+
+usage() {
+    cat <<EOF
+usage: $(basename $0) [-h] {check,nginx,ingress,deployed,version,/bin/sh}
+
+positional arguments:
+  {check,nginx,ingress,deployed,version,/bin/sh}
+                        choose action to run
+
+optional arguments:
+  -h, --help            show this help message and exit
+EOF
+}
+
+case "$ACTION" in
+    check)
+        echo "check: nginx -t"
+        exec nginx -t
+        ;;
+    nginx)
+        echo "nginx: nginx -g daemon off;"
+        exec nginx -g "daemon off;"
+        ;;
+    ingress)
+        echo "ingress: kubectl apply -f $DIR/ingress.yaml"
+        exec kubectl apply -f $DIR/ingress.yaml
+        ;;
+    deployed)
+        echo "cat $DIR/deployed"
+        cat $DIR/deployed
+        ;;
+    version)
+        echo "cat $DIR/version"
+        cat $DIR/version
+        ;;
+    /bin/sh)
+        echo "/bin/sh"
+        exec /bin/sh
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+esac
+

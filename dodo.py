25436a6df0abde124e5504a088d4652a622da6ec
Scott Idler <sidler@mozilla.com>
2020-05-30 12:19:21 -0700

    adjust docker image and add deployed, version json
    - created refractr/image/
    - moved refractr/refractr.sh -> refractr/image/refractr.sh
    - added cfg.py to handle config values and allow env var passthrus
    - added deployed and version tasks to create json files in image/
    - Dockerfile to COPY image/ -> /refractr/; nginx/ -> /etc/nginx/
    - updated Dockerfile ENTRYPOINT /refractr/refractr.sh
    - moved logic into SHOULD_PUBLISH property
    - simplified refractr image name in docker-compose
    - publish now creates a tag for the ecr repository name
    - .gitignore for generated files: deployed, version, ingress.yaml

--- dodo.py
+++ dodo.py
@@ -5,42 +5,20 @@ import re
 import json
 
 from doit.tools import LongRunning
-from subprocess import check_output, CalledProcessError, PIPE
+from functools import lru_cache
 from ruamel.yaml import safe_load
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 
-DIR = os.path.abspath(os.path.dirname(__file__))
-CWD = os.path.abspath(os.getcwd())
-REL = os.path.relpath(DIR, CWD)
-REFRACTR = f'{REL}/refractr'
-REFRACTR_YML = os.getenv('REFRACTR_YML', f'{REFRACTR}/refractr.yml')
-SCHEMA_YML = os.getenv('SCHEMA_YML', f'{REFRACTR}/schema.yml')
-NGINX = f'{REFRACTR}/nginx'
+from refractr.cfg import CFG, git, call, CalledProcessError
+
 IMAGE = 'itsre/refractr'
 CREDENTIALS_MESSAGE = 'Unable to locate credentials. You can configure credentials by running "aws configure".'
-INGRESS_YAML_TEMPLATE = f'{REFRACTR}/ingress.yaml.template'
-PROD_TAG_PN = '$(v[0-9]+.[0-9]+.[0-9]+)$'
-TRAVIS = os.getenv('TRAVIS')
-TRAVIS_TAG = os.getenv('TRAVIS_TAG')
-TRAVIS_BRANCH = os.getenv('TRAVIS_BRANCH')
-TRAVIS_PULL_REQUEST = os.getenv('TRAVIS_PULL_REQUEST')
-PUBLISH_BRANCHES = [
-    'master',
-]
 
 DOIT_CONFIG = {
     'default_tasks': ['test'],
     'verbosity': 2,
 }
-
-def call(cmd, stderr=PIPE, shell=True, **kwargs):
-    result = check_output(
-        cmd,
-        stderr=stderr,
-        shell=shell,
-        **kwargs).decode('utf-8').rstrip()
-    return result
 
 def branch_contains(tag, approved):
     '''
@@ -48,48 +26,23 @@ def branch_contains(tag, approved):
     '''
     cmd = f'git branch --contains "{tag}"'
     try:
-        lines = call(cmd).split('\n')
+        _, stdout, _ =  call(cmd)
+        lines = stdout.split('\n')
         branches = [line[2:] for line in lines]
         return not set(branches).isdisjoint(approved)
     except CalledProcessError as cpe:
         return False
 
-def aws_account():
-    cmd = 'aws sts get-caller-identity'
-    result = call(cmd)
-    obj = json.loads(result)
-    return obj['Account']
-
-def reponame():
-    cmd = 'basename $(git rev-parse --show-toplevel)'
-    result = call(cmd, cwd=REL)
-    return result
-
-AWS_REGION = os.getenv('AWS_REGION', 'us-west-2')
-AWS_ACCOUNT = os.getenv('AWS_ACCOUNT', aws_account())
-REPONAME = os.getenv('REPONAME', reponame())
-REPOURL = f'{AWS_ACCOUNT}.dkr.ecr.{AWS_REGION}.amazonaws.com/{REPONAME}'
-REFRACTR_VERSION = call('git describe --match "v*" --abbrev=7')
-
+@lru_cache()
 def envs(sep=' ', **kwargs):
     envs = dict(
-        REFRACTR_VERSION=REFRACTR_VERSION,
-        AWS_REGION=AWS_REGION,
-        AWS_ACCOUNT=AWS_ACCOUNT,
+        REFRACTR_VERSION=CFG.VERSION,
+        AWS_REGION=CFG.AWS_REGION,
+        AWS_ACCOUNT=CFG.AWS_ACCOUNT,
     )
     return sep.join(
         [f'{key}={value}' for key, value in sorted(dict(envs, **kwargs).items())]
     )
-
-def check_creds():
-    cmd = 'aws sts get-caller-identity'
-    try:
-        call(cmd)
-        return True
-    except CalledProcessError as cpe:
-        if CREDENTIALS_MESSAGE in cpe.stderr.decode():
-            return False
-        raise cpe
 
 def task_creds():
     '''
@@ -99,7 +52,43 @@ def task_creds():
         'actions': [
             f'echo "{CREDENTIALS_MESSAGE}"',
         ],
-        'uptodate': [check_creds],
+        'uptodate': [lambda: CFG.IS_AUTHORIZED],
+    }
+
+def write_json(filename, **items):
+    with open(filename, 'w') as f:
+        f.write(json.dumps(items, indent=4, sort_keys=True))
+
+def task_deployed():
+    '''
+    write refractr/deployed json file
+    '''
+    items = dict(
+        DEPLOYED_BY=CFG.COMMITTED_BY,
+        DEPLOYED_ENV=CFG.DEPLOYED_ENV,
+        DEPLOYED_WHEN=CFG.DEPLOYED_WHEN,
+    )
+    if CFG.COMMITTED_BY != CFG.AUTHORED_BY:
+        items.update(AUTHORED_BY=CFG.AUTHORED_BY)
+    return {
+        'actions': [
+            lambda: write_json(f'{CFG.IMAGE}/deployed', **items)
+        ],
+    }
+
+def task_version():
+    '''
+    write refractr/version json file
+    '''
+    items = dict(
+        BRANCH=CFG.BRANCH,
+        REVISION=CFG.REVISION,
+        VERSION=CFG.VERSION,
+    )
+    return {
+        'actions': [
+            lambda: write_json(f'{CFG.IMAGE}/version', **items)
+        ],
     }
 
 def task_schema():
@@ -107,12 +96,12 @@ def task_schema():
     test refractr.yml against schema.yml using jsonschema
     '''
     def schema():
-        assert os.path.isfile(REFRACTR_YML)
-        assert os.path.isfile(SCHEMA_YML)
-        print(f'validating {REFRACTR_YML} against {SCHEMA_YML} =>', end=' ')
-        with open(REFRACTR_YML, 'r') as f:
+        assert os.path.isfile(CFG.REFRACTR_YML)
+        assert os.path.isfile(CFG.SCHEMA_YML)
+        print(f'validating {CFG.REFRACTR_YML} against {CFG.SCHEMA_YML} =>', end=' ')
+        with open(CFG.REFRACTR_YML, 'r') as f:
             refractr_yml = safe_load(f)
-        with open(SCHEMA_YML, 'r') as f:
+        with open(CFG.SCHEMA_YML, 'r') as f:
             schema_yml = safe_load(f)
         try:
             validate(refractr_yml, schema_yml)
@@ -134,7 +123,7 @@ def task_nginx():
     '''
     generate nginx.conf files from refractr.yml
     '''
-    cmd = f'bin/refractr nginx > {NGINX}/conf.d/refractr.conf'
+    cmd = f'bin/refractr nginx > {CFG.NGINX}/conf.d/refractr.conf'
     return {
         'task_dep': [
             'schema',
@@ -149,7 +138,8 @@ def task_ingress():
     '''
     create ingress.yaml from refractr.yml domains and ingress.yaml.template
     '''
-    cmd = f'bin/refractr ingress --ingress-template {INGRESS_YAML_TEMPLATE} > {INGRESS_YAML_TEMPLATE.replace(".template", "")}'
+    ingress_yaml = os.path.basename(CFG.INGRESS_YAML_TEMPLATE.replace('.template', ''))
+    cmd = f'bin/refractr ingress --ingress-template {CFG.INGRESS_YAML_TEMPLATE} > {CFG.IMAGE}/{ingress_yaml}'
     return {
         'task_dep': [
             'schema',
@@ -166,6 +156,8 @@ def task_build():
     '''
     return {
         'task_dep': [
+            'deployed',
+            'version',
             'creds',
             'nginx',
             'ingress',
@@ -225,7 +217,7 @@ def task_login():
     '''
     perform ECR docker login via AWS perms
     '''
-    cmd = f'aws ecr get-login-password --region {AWS_REGION} | docker login --username AWS --password-stdin {REPOURL}'
+    cmd = f'aws ecr get-login-password --region {CFG.AWS_REGION} | docker login --username AWS --password-stdin {CFG.ECR_REPOURL}'
     return {
         'task_dep': [
             'creds',
@@ -240,25 +232,17 @@ def task_publish():
     publish docker image to aws ECR
     '''
     def should_publish():
-        publish = False
         print(' '.join([
-            f'TRAVIS={TRAVIS}',
-            f'TRAVIS_TAG={TRAVIS_TAG}',
-            f'TRAVIS_BRANCH={TRAVIS_BRANCH}',
-            f'TRAVIS_PULL_REQUEST={TRAVIS_PULL_REQUEST}',
-            f'branch_contains={branch_contains(TRAVIS_TAG, PUBLISH_BRANCHES)}',
+            f'TRAVIS={CFG.TRAVIS}',
+            f'TRAVIS_TAG={CFG.TRAVIS_TAG}',
+            f'TRAVIS_BRANCH={CFG.TRAVIS_BRANCH}',
+            f'TRAVIS_PULL_REQUEST={CFG.TRAVIS_PULL_REQUEST}',
+            f'DEPLOYED_ENV={CFG.DEPLOYED_ENV}',
+            f'branch_contains={branch_contains(CFG.TRAVIS_TAG, CFG.PUBLISH_BRANCHES)}',
         ]))
-        if TRAVIS:
-            if TRAVIS_PULL_REQUEST == "false":
-                if TRAVIS_TAG and branch_contains(TRAVIS_TAG, PUBLISH_BRANCHES):
-                    publish = True
-                elif TRAVIS_BRANCH and TRAVIS_BRANCH in PUBLISH_BRANCHES:
-                    publish = True
-        elif not re.search(PROD_TAG_PN, REFRACTR_VERSION):
-            publish = True
-        print(f'publishing {REFRACTR_VERSION if publish else "skipped"}')
-        return publish
-
+        if CFG.SHOULD_PUBLISH:
+            print(f'publishing {CFG.VERSION if publish else "skipped"}')
+        return CFG.SHOULD_PUBLISH
     return {
         'task_dep': [
             'test',
@@ -266,7 +250,8 @@ def task_publish():
             'login',
         ],
         'actions': [
-            f'env {envs()} docker-compose push refractr',
+            f'docker tag refractr:{CFG.VERSION} {CFG.IMAGE_NAME_AND_TAG}',
+            f'docker push {CFG.IMAGE_NAME_AND_TAG}',
         ],
         # inverse to tell doit task not uptodate, therefore publish
         'uptodate': [lambda: not should_publish()],
---
+++ refractr/cfg.py
@@ -0,0 +1,263 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import os
+import re
+import json
+
+from datetime import datetime
+from functools import lru_cache
+from decouple import UndefinedValueError, AutoConfig, config
+from subprocess import Popen, CalledProcessError, PIPE  # nosec
+
+DIR = os.path.abspath(os.path.dirname(__file__))
+CWD = os.path.abspath(os.getcwd())
+REL = os.path.relpath(DIR, CWD)
+
+CREDENTIALS_MESSAGE = 'Unable to locate credentials. You can configure credentials by running "aws configure".'
+
+class NotGitRepoError(Exception):
+    def __init__(self, cwd=os.getcwd()):
+        msg = f"not a git repository error cwd={cwd}"
+        super().__init__(msg)
+
+
+class GitCommandNotFoundError(Exception):
+    def __init__(self):
+        msg = "git: command not found"
+        super().__init__(msg)
+
+def call(
+    cmd,
+    stdout=PIPE,
+    stderr=PIPE,
+    shell=True,
+    nerf=False,
+    throw=True,
+    verbose=False):
+
+    if nerf:
+        return (None, "nerfed", "nerfed")
+    process = Popen(cmd, stdout=stdout, stderr=stderr, shell=shell)  # nosec
+    _stdout, _stderr = [
+        stream.decode("utf-8") if stream is not None else None
+        for stream in process.communicate()
+    ]
+    exitcode = process.poll()
+    if throw and exitcode:
+        raise CalledProcessError(
+            exitcode, f"cmd={cmd}; stdout={_stdout}; stderr={_stderr}"
+        )
+    return exitcode, _stdout, _stderr
+
+def git(args, strip=True, **kwargs):
+    try:
+        _, stdout, stderr = call("git rev-parse --is-inside-work-tree")
+    except CalledProcessError as ex:
+        if "not a git repository" in str(ex):
+            raise NotGitRepoError
+        elif "git: command not found" in str(ex):
+            raise GitCommandNotFoundError
+    try:
+        _, result, _ = call(f"git {args}", **kwargs)
+        if result:
+            result = result.strip()
+        return result
+    except CalledProcessError as ex:
+        raise ex
+
+
+class AutoConfigPlus(AutoConfig):  # pylint: disable=too-many-public-methods
+
+    @property
+    def IS_AUTHORIZED(self):
+        try:
+            call('aws sts get-caller-identity')
+        except CalledProcessError as cpe:
+            if CREDENTIALS_MESSAGE in cpe.stderr.decode():
+                return False
+            raise cpe
+
+    @property
+    @lru_cache()
+    def REPONAME(self):
+        result = git('rev-parse --show-toplevel')
+        reponame = os.path.basename(result)
+        return self('REPONAME', reponame)
+
+    @property
+    @lru_cache()
+    def AWS_ACCOUNT(self):
+        _, stdout, _ = call('aws sts get-caller-identity')
+        obj = json.loads(stdout)
+        return obj['Account']
+
+    @property
+    @lru_cache()
+    def AWS_REGION(self):
+        return self('AWS_REGION', 'us-west-2')
+
+    @property
+    @lru_cache()
+    def ECR_REPOURL(self):
+        ecr_repourl = f'{self.AWS_ACCOUNT}.dkr.ecr.{self.AWS_REGION}.amazonaws.com/{self.REPONAME}'
+        return self('ECR_REPOURL', ecr_repourl)
+
+    @property
+    @lru_cache()
+    def IMAGE_NAME_AND_TAG(self):
+        return self('IMAGE_NAME_AND_TAG', f'{self.ECR_REPOURL}/{self.REPONAME}:{self.VERSION}')
+
+    @property
+    @lru_cache()
+    def TRAVIS(self):
+        return self('TRAVIS', False)
+
+    @property
+    @lru_cache()
+    def TRAVIS_TAG(self):
+        return self('TRAVIS_TAG')
+
+    @property
+    @lru_cache()
+    def TRAVIS_BRANCH(self):
+        return self('TRAVIS_BRANCH')
+
+    @property
+    @lru_cache()
+    def TRAVIS_PULL_REQUEST(self):
+        return self('TRAVIS_PULL_REQUEST')
+
+    @property
+    @lru_cache()
+    def REFRACTR(self):
+        return self('REFRACTR', REL)
+
+    @property
+    @lru_cache()
+    def REFRACTR_YML(self):
+        return self('REFRACTR_YML', f'{self.REFRACTR}/refractr.yml')
+
+    @property
+    @lru_cache()
+    def SCHEMA_YML(self):
+        return self('SCHEMA_YML', f'{self.REFRACTR}/schema.yml')
+
+    @property
+    @lru_cache()
+    def INGRESS_YAML_TEMPLATE(self):
+        return self('INGRESS_YAML_TEMPLATE', f'{self.REFRACTR}/ingress.yaml.template')
+
+    @property
+    @lru_cache()
+    def IMAGE(self):
+        return self('IMAGE', f'{self.REFRACTR}/image')
+
+    @property
+    @lru_cache()
+    def NGINX(self):
+        return self('NGINX', f'{self.REFRACTR}/nginx')
+
+    @property
+    @lru_cache()
+    def VERSION(self):
+        try:
+            return git("describe --abbrev=7 --always")
+        except (NotGitRepoError, GitCommandNotFoundError):
+            return self("VERSION")
+
+    @property
+    @lru_cache()
+    def BRANCH(self):
+        try:
+            return git("rev-parse --abbrev-ref HEAD")
+        except (NotGitRepoError, GitCommandNotFoundError):
+            return self("BRANCH")
+
+    @property
+    @lru_cache()
+    def PUBLISH_BRANCHES(self):
+        branches = self('PUBLISH_BRANCHES', None)
+        if branches:
+            return branches.split(',')
+        return ['master']
+
+    @property
+    @lru_cache()
+    def SHOULD_PUBLISH(self):
+        publish = False
+        if self.TRAVIS:
+            if self.TRAVIS_PULL_REQUEST == "false":
+                if self.TRAVIS_TAG and branch_contains(self.TRAVIS_TAG, self.PUBLISH_BRANCHES):
+                    publish = True
+                elif self.TRAVIS_BRANCH and self.TRAVIS_BRANCH in self.PUBLISH_BRANCHES:
+                    publish = True
+        elif not re.search(self.PROD_TAG_PATTERN, self.VERSION):
+            publish = True
+        return publish
+
+    @property
+    @lru_cache()
+    def PROD_TAG_PATTERN(self):
+        return self('PROD_TAG_PATTERN', '^(v[0-9]+\.[0-9]+\.[0-9]+)$')
+
+    @property
+    @lru_cache()
+    def DEPLOYED_ENV(self):
+        if self.TRAVIS:
+            match = re.search(self.PROD_TAG_PATTERN, self.VERSION)
+            if match:
+                return 'prod'
+            else:
+                return 'stage'
+        return 'dev'
+
+    @property
+    @lru_cache()
+    def AUTHOR_NAME(self):
+        author_name = git(f'log -1 {self.REVISION} --pretty="%aN"')
+        return self('AUTHOR_NAME', author_name)
+
+    @property
+    @lru_cache()
+    def AUTHOR_EMAIL(self):
+        author_email = git(f'log -1 {self.REVISION} --pretty="%aE"')
+        return self('AUTHOR_EMAIL', author_email)
+
+    @property
+    @lru_cache()
+    def COMMITTER_NAME(self):
+        committer_name = git(f'log -1 {self.REVISION} --pretty="%cN"')
+        return self('COMMITTER_NAME', committer_name)
+
+    @property
+    @lru_cache()
+    def COMMITTER_EMAIL(self):
+        committer_email = git(f'log -1 {self.REVISION} --pretty="%cE"')
+        return self('COMMITTER_EMAIL', committer_email)
+
+    @property
+    @lru_cache()
+    def AUTHORED_BY(self):
+        return self('AUTHORED_BY', f'{self.AUTHOR_NAME} <{self.AUTHOR_EMAIL}>')
+
+    @property
+    @lru_cache()
+    def COMMITTED_BY(self):
+        return self('COMMITTED_BY', f'{self.COMMITTER_NAME} <{self.COMMITTER_EMAIL}>')
+
+    @property
+    @lru_cache()
+    def DEPLOYED_WHEN(self):
+        return self("DEPLOYED_WHEN", datetime.utcnow().isoformat())
+
+    @property
+    @lru_cache()
+    def REVISION(self):
+        try:
+            return git("rev-parse HEAD")
+        except (NotGitRepoError, GitCommandNotFoundError):
+            return self("REVISION")
+
+CFG = AutoConfigPlus()

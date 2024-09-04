0a759a5a5f59d8b0d7039e1d4531518acc4289ce
André Honeiser <ahoneiser@mozilla.com>
2024-09-04 15:31:23 +0200

    chore: refine docker image

--- refractr/image/refractr.sh
+++ refractr/image/refractr.sh
@@ -7,10 +7,10 @@ a="/$0"; a=${a%/*}; a=${a#/}; a=${a:-.}; DIR=$(cd "$a"; pwd)
 
 usage() {
     cat <<EOF
-usage: $SCRIPT [-h] {check,nginx,ingress,deployed,version,/bin/sh}
+usage: $SCRIPT [-h] {check,nginx,deployed,version,/bin/sh}
 
 positional arguments:
-  {check,nginx,ingress,deployed,version,/bin/sh}
+  {check,nginx,deployed,version,/bin/sh}
                         choose action to run
 
 optional arguments:
@@ -26,10 +26,6 @@ case "$ACTION" in
     nginx)
         echo "nginx: nginx -g daemon off;"
         exec nginx -g "daemon off;"
-        ;;
-    ingress)
-        echo "ingress: kubectl apply -f $DIR/ingress.yaml"
-        exec kubectl apply -f $DIR/ingress.yaml
         ;;
     deployed)
         echo "cat $DIR/deployed"
